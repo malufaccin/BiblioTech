a53497799382e5773fc59420f12d4f6e3c8732a9
malufaccin <maludfaccin@gmail.com>
2025-02-02 19:39:17 -0300

    upload e js

--- script.js
+++ script.js
@@ -17,24 +17,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     const books = [
-        { title: "Amar é assim", author: "Dolly Alderton", epub: "./ebook/Amar é assim.epub" },
-        { title: "Amor em Roma", author: "Sarah Adams", epub: "./ebook/Amor em Roma.epub" },
+        { title: "Amar é assim", author: "Dolly Alderton", epub: "./ebook/Amar_e_assim.epub" },
+        { title: "Amor em Roma", author: "Sarah Adams", epub: "./ebook/Amor_em_Roma.epub" },
         { title: "Apenas amigos", author: "Abby Jimenez", epub: "./ebook/Apenas amigos.epub" },
-        { title: "Cleópatra e Frankenstein", author: "Coco Mellors", epub: "./ebook/Cleópatra e Frankenstein.epub" },
-        { title: "Em agosto nos vemos", author: "Gabriel García Márquez", epub: "./ebook/Em agosto nos vemos.epub" },
-        { title: "Flores Para Algernon", author: "Daniel Keys", epub: "./ebook/Flores Para Algernon.epub" },
-        { title: "Leitura de Verão", author: "Emily Henry", epub: "./ebook/Leitura de Verão .epub" },
-        { title: "Minha melhor parte", author: "Hannah Bonam-Young", epub: "./ebook/Minha melhor parte.epub" },
-        { title: "Nem te conto", author: "Emily Henry", epub: "./ebook/Nem te conto.epub" },
-        { title: "O estrangeiro", author: "Albert Camus", epub: "./ebook/O estrangeiro.epub" },
-        { title: "O Jardim Secreto", author: "Frances Hodgson Burnett", epub: "./ebook/O Jardim Secreto.epub" },
-        { title: "Ontem à noite", author: "Mhairi McFarlane", epub: "./ebook/Ontem a noite.epub" },
-        { title: "Os Sete Maridos de Evelyn Hugo", author: "Taylor Jenkins Reid", epub: "./ebook/Os Sete maridos de Evelyn Hugo.epub" },
-        { title: "Parte do seu mundo", author: "Abby Jimenez", epub: "./ebook/Parte do seu mundo.epub" },
-        { title: "Pessoas normais", author: "Sally Rooney", epub: "./ebook/Pessoas normais.epub" },
-        { title: "Playlist para um final feliz", author: "Abby Jimenez", epub: "./ebook/Playlist para um final feliz.epub" },
-        { title: "Sinais do amor", author: "Hannah Bonam-Young", epub: "./ebook/Sinais do amor.epub" },
-        { title: "Três", author: "Valerie Perrin", epub: "./ebook/Três.epub" }
+        { title: "Cleópatra e Frankenstein", author: "Coco Mellors", epub: "./ebook/Cleópatra_e_Frankenstein.epub" },
+        { title: "Em agosto nos vemos", author: "Gabriel García Márquez", epub: "./ebook/Em_agosto_nos_vemos.epub" },
+        { title: "Flores Para Algernon", author: "Daniel Keys", epub: "./ebook/Flores_Para_Algernon.epub" },
+        { title: "Leitura de Verão", author: "Emily Henry", epub: "./ebook/Leitura_de_Verão.epub" },
+        { title: "Minha melhor parte", author: "Hannah Bonam-Young", epub: "./ebook/Minha_melhor_parte.epub" },
+        { title: "Nem te conto", author: "Emily Henry", epub: "./ebook/Nem_te_conto.epub" },
+        { title: "O estrangeiro", author: "Albert Camus", epub: "./ebook/O_estrangeiro.epub" },
+        { title: "O Jardim Secreto", author: "Frances Hodgson Burnett", epub: "./ebook/O_Jardim_Secreto.epub" },
+        { title: "Ontem à noite", author: "Mhairi McFarlane", epub: "./ebook/Ontem_a_noite.epub" },
+        { title: "Os Sete Maridos de Evelyn Hugo", author: "Taylor Jenkins Reid", epub: "./ebook/Os_Sete_maridos_de_Evelyn_Hugo.epub" },
+        { title: "Parte do seu mundo", author: "Abby Jimenez", epub: "./ebook/Parte_do_seu_mundo.epub" },
+        { title: "Pessoas normais", author: "Sally Rooney", epub: "./ebook/Pessoas_normais.epub" },
+        { title: "Playlist para um final feliz", author: "Abby Jimenez", epub: "./ebook/Playlist_para_um_final_feliz.epub" },
+        { title: "Sinais do amor", author: "Hannah Bonam-Young", epub: "./ebook/Sinais_do_amor.epub" },
+        { title: "Três", author: "Valerie Perrin", epub: "./ebook/Tres.epub" }
     ];
 
     const bookList = document.getElementById("bookList");
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
             bookElement.innerHTML = `
                 <h3>${book.title}</h3>
                 <p>${book.author}</p>
-                <a href="./ebook/${book.epub}" download>
+                <a href="${book.epub}" download="/ebook/${book.title}.epub">
                     <button>epub</button>
                 </a>
             `;
